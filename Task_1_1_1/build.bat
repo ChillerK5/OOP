52b1f95813d143d31dfac510a15ffdb7ed46446a
ChillerK5 <klimbagr@mail.ru>
2022-09-19 00:59:41 +0700

    Rewritten *.bat file

--- Task_1_1_1/build.bat
+++ Task_1_1_1/build.bat
@@ -1,6 +1,6 @@
-mkdir .\build_shell\classes
-mkdir .\build_shell\docs
-mkdir .\build_shell\jar\
-javac -d .\build_shell\classes .\src\main\java\ru\nsu\kbagryantsev\Heapsort.java
-javadoc -d .\build_shell\doc\ -charset utf-8 -sourcepath .\src\main\java\ -subpackages ru.nsu.kbagryantsev
-jar cf .\build_shell\jar\HeapSort.jar .\src\main\java\ru\nsu\kbagryantsev\Heapsort.java
+mkdir .\bin\classes
+mkdir .\bin\docs
+mkdir .\bin\jar\
+javac -d .\bin\classes .\src\main\java\ru\nsu\kbagryantsev\Heapsort.java
+javadoc -d .\bin\doc\ -charset utf-8 -sourcepath .\src\main\java\ -subpackages ru.nsu.kbagryantsev
+jar cf .\bin\jar\HeapSort.jar .\src\main\java\ru\nsu\kbagryantsev\Heapsort.java
