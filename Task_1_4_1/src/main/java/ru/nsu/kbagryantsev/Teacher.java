a4584f6f96ab0048a9c2879a4e98ede6b2ac7723
ChillerK5 <klimbagr@mail.ru>
2022-11-21 18:27:37 +0700

    Create draft package structure

---
+++ Task_1_4_1/src/main/java/ru/nsu/kbagryantsev/Teacher.java
@@ -0,0 +1,13 @@
+package ru.nsu.kbagryantsev;
+
+public class Teacher {
+    String firstName;
+    String lastName;
+    String patronymic;
+
+    public Teacher(String firstName, String lastName, String patronymic) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+    }
+}
