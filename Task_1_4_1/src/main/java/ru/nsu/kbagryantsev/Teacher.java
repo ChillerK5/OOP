841695a27022429d20aa70f71e4d4bff356fcb56
ChillerK5 <klimbagr@mail.ru>
2022-12-20 00:49:20 +0700

    Replace record wrapper with canonical class declaration

--- Task_1_4_1/src/main/java/ru/nsu/kbagryantsev/Teacher.java
+++ Task_1_4_1/src/main/java/ru/nsu/kbagryantsev/Teacher.java
@@ -1,24 +1,70 @@
 package ru.nsu.kbagryantsev;
 
+import java.util.Objects;
+
 /**
  * Record class teacher assigned with teacher's ids and his signature.
  */
-public record Teacher(String firstName, String lastName, String patronymic) {
+public final class Teacher {
+    private final String firstName;
+    private final String lastName;
+    private final String patronymic;
+
     /**
-     * @param firstName firstName
-     * @param lastName lastName
+     * @param firstName  firstName
+     * @param lastName   lastName
      * @param patronymic patronymic
      */
-    public Teacher {
+    public Teacher(String firstName, String lastName, String patronymic) {
 
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
     }
+
     /**
      * Support for a case of patromynic absense.
      *
      * @param firstName first name
-     * @param lastName last name
+     * @param lastName  last name
      */
     Teacher(final String firstName, final String lastName) {
         this(firstName, lastName, null);
     }
+
+    public String firstName() {
+        return firstName;
+    }
+
+    public String lastName() {
+        return lastName;
+    }
+
+    public String patronymic() {
+        return patronymic;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Teacher) obj;
+        return Objects.equals(this.firstName, that.firstName) &&
+                Objects.equals(this.lastName, that.lastName) &&
+                Objects.equals(this.patronymic, that.patronymic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, patronymic);
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher[" +
+                "firstName=" + firstName + ", " +
+                "lastName=" + lastName + ", " +
+                "patronymic=" + patronymic + ']';
+    }
+
 }
