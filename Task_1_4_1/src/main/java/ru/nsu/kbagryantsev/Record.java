366dfbfc91799d04030377775aa4cea0445be398
ChillerK5 <klimbagr@mail.ru>
2022-12-21 14:05:37 +0700

    Force gradle to support JDK 18 Convert Record and Teacher to record

--- Task_1_4_1/src/main/java/ru/nsu/kbagryantsev/Record.java
+++ Task_1_4_1/src/main/java/ru/nsu/kbagryantsev/Record.java
@@ -1,68 +1,11 @@
 package ru.nsu.kbagryantsev;
 
-import java.util.Objects;
-
 /**
  * Single credit book record. Contains a subject, a teacher tutoring it and a
  * received grade.
+ *
+ * @param subject subject
+ * @param teacher teacher
+ * @param grade grade
  */
-public final class Record {
-    private final Subject subject;
-    private final Teacher teacher;
-    private final int grade;
-
-    /**
-     * Instantiates a record with given data.
-     *
-     * @param subject subject
-     * @param teacher teacher
-     * @param grade   grade
-     */
-    public Record(Subject subject, Teacher teacher, int grade) {
-        this.subject = subject;
-        this.teacher = teacher;
-        this.grade = grade;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null || obj.getClass() != this.getClass()) {
-            return false;
-        }
-        var that = (Record) obj;
-        return Objects.equals(this.subject, that.subject)
-                && Objects.equals(this.teacher, that.teacher)
-                && this.grade == that.grade;
-    }
-
-    @Override
-    public String toString() {
-        return "Record["
-                +
-                "subject=" + subject + ", "
-                +
-                "teacher=" + teacher + ", "
-                +
-                "grade=" + grade + ']';
-    }
-
-    public Subject subject() {
-        return subject;
-    }
-
-    public Teacher teacher() {
-        return teacher;
-    }
-
-    public int grade() {
-        return grade;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(subject, teacher, grade);
-    }
-}
+public record Record(Subject subject, Teacher teacher, int grade) { }
--- Task_1_4_1/src/main/java/ru/nsu/kbagryantsev/Teacher.java
+++ Task_1_4_1/src/main/java/ru/nsu/kbagryantsev/Teacher.java
@@ -1,28 +1,13 @@
 package ru.nsu.kbagryantsev;
 
-import java.util.Objects;
-
 /**
  * Record class teacher assigned with teacher's ids and his signature.
+ *
+ * @param firstName firstName
+ * @param lastName lastName
+ * @param patronymic patronymic
  */
-public final class Teacher {
-    private final String firstName;
-    private final String lastName;
-    private final String patronymic;
-
-    /**
-     * Instantiates the teacher by his names.
-     *
-     * @param firstName  firstName
-     * @param lastName   lastName
-     * @param patronymic patronymic
-     */
-    public Teacher(String firstName, String lastName, String patronymic) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.patronymic = patronymic;
-    }
-
+public record Teacher(String firstName, String lastName, String patronymic) {
     /**
      * Support for a case of patromynic absense.
      *
@@ -32,47 +17,4 @@ public final class Teacher {
     Teacher(final String firstName, final String lastName) {
         this(firstName, lastName, null);
     }
-
-    public String firstName() {
-        return firstName;
-    }
-
-    public String lastName() {
-        return lastName;
-    }
-
-    public String patronymic() {
-        return patronymic;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null || obj.getClass() != this.getClass()) {
-            return false;
-        }
-        var that = (Teacher) obj;
-        return Objects.equals(this.firstName, that.firstName)
-                && Objects.equals(this.lastName, that.lastName)
-                && Objects.equals(this.patronymic, that.patronymic);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, patronymic);
-    }
-
-    @Override
-    public String toString() {
-        return "Teacher["
-                +
-                "firstName=" + firstName + ", "
-                +
-                "lastName=" + lastName + ", "
-                +
-                "patronymic=" + patronymic + ']';
-    }
-
 }
