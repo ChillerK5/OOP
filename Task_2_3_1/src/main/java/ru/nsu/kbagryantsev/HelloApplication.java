edf229d34f17b1948d4d0177f23f53bdfd95b4ac
ChillerK5 <klimbagr@mail.ru>
2023-04-11 22:42:56 +0700

    Fix reviewdog issues

--- Task_2_3_1/src/main/java/ru/nsu/kbagryantsev/HelloApplication.java
+++ Task_2_3_1/src/main/java/ru/nsu/kbagryantsev/HelloApplication.java
@@ -1,23 +1,32 @@
 package ru.nsu.kbagryantsev;
 
+import java.io.IOException;
+import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class HelloApplication extends Application {
+/**
+ * HelloApplication.
+ */
+public final class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    public void start(final Stage stage) throws IOException {
+        URL url = HelloApplication.class.getResource("hello-view.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(url);
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    /**
+     * Main.
+     *
+     * @param args args
+     */
+    public static void main(final String[] args) {
         launch();
     }
 }
--- Task_2_3_1/src/main/java/ru/nsu/kbagryantsev/HelloController.java
+++ Task_2_3_1/src/main/java/ru/nsu/kbagryantsev/HelloController.java
@@ -3,12 +3,18 @@ package ru.nsu.kbagryantsev;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/**
+ * HelloController.
+ */
 public class HelloController {
+    /**
+     * welcomeText.
+     */
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected final void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
